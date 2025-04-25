0c81d4dfa78881cece3ea488d2be40ea6682d7bb
israfil-miya <israfilmiya120@gmail.com>
2025-04-25 20:18:12 +0600

    ui: featured & works

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -18,7 +18,7 @@ function Nav() {
   return (
     <>
       {/* Navbar */}
-      <div className="flex items-center align-middle justify-between bg-background text-foreground p-1 px-8 md:px-16 lg:px-24 xl:px-32">
+      <div className="flex bg-gray-50 items-center align-middle justify-between text-foreground p-1 px-4">
         <div className="flex items-center space-x-4">
           <Image
             src="/images/csag-logo-no-bg.png"
@@ -26,14 +26,14 @@ function Nav() {
             width={80}
             height={80}
           />
-          <h2 className="text-xl font-bold tracking-wide">
+          <h2 className="text-2xl font-bold tracking-wide">
             <span className="md:block hidden uppercase font-sans">
               Child Survival Aid Ghana
             </span>
             <span className="md:hidden block">CSAG</span>
           </h2>
         </div>
-        <div className="items-center text-md gap-x-16 uppercase font-semibold xl:flex hidden">
+        <div className="items-center text-lg gap-x-16 uppercase font-semibold xl:flex hidden">
           <nav className="space-x-5 flex flex-row items-center">
             <Link
               href="/"
@@ -89,7 +89,7 @@ function Nav() {
       </div>
 
       {/* Hero Section with Background Image */}
-      <div className="relative flex items-end pb-24 px-10 justify-start min-h-[70vh] xl:min-h-[70vh] bg-gray-900 overflow-hidden">
+      <div className="relative flex items-end pb-24 px-10 justify-start min-h-[70vh] xl:min-h-[70vh] text-gray-800 overflow-hidden">
         <Image
           src="/images/photoshoot/reading-book-in-library.JPG"
           alt="Background library scene"
@@ -116,7 +116,7 @@ function Nav() {
       </div>
 
       {/* About Section */}
-      <div className="p-1 px-8 md:px-16 lg:px-24 xl:px-32 mt-10">
+      <div className="p-1 mt-16">
         <p className="leading-relaxed mb-4 text-lg container">
           <span className="text-lg font-bold text-accent font-sans">
             Child Survival Aid Ghana (CSAG)
@@ -140,48 +140,199 @@ function Nav() {
       </div>
 
       {/* Impact banner */}
-      <div className="flex flex-col items-center justify-center bg-gray-100 text-gray-800 py-10 px-8 md:px-16 lg:px-24 xl:px-32 mt-10">
-        <div className="text-center leading-snug">
-          <h3 className="text-3xl  font-bold mb-2 uppercase tracking-wide">
-            IMPACT WE MADE
-          </h3>
-          {/* <p className="text-md text-gray-200 tracking-wide">
+      <div className="flex flex-col border-y-2 border-slate-300 items-center justify-center bg-gray-100 text-gray-800 py-10 mt-16">
+        <div className="container">
+          <div className="text-center leading-snug">
+            <h3 className="text-3xl  font-bold mb-2 uppercase tracking-wide">
+              IMPACT WE MADE
+            </h3>
+            {/* <p className="text-md text-gray-200 tracking-wide">
             Together, we&apos;ve helped build brighter futures for children
             across Ghana
           </p> */}
+          </div>
+          {/* Stats Grid */}
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-8">
+            {/* Stat 1 */}
+            <div className="text-center">
+              <p className="text-4xl md:text-5xl lg:text-6xl font-semibold text-accent mb-3 hover:underline hover:underline-offset-4 hover:decoration-6 hover:decoration-accent">
+                875,000+
+              </p>
+              <p className="text-sm md:text-lg uppercase tracking-wide font-mono">
+                Children reached through our educational programs and healthcare
+                initiatives
+              </p>
+            </div>
+
+            {/* Stat 2 */}
+            <div className="text-center">
+              <p className="text-4xl md:text-5xl lg:text-6xl font-semibold text-accent mb-3 hover:underline hover:underline-offset-4 hover:decoration-6 hover:decoration-accent">
+                600+
+              </p>
+              <p className="text-sm md:text-lg uppercase tracking-wide font-mono">
+                Schools supported through construction, renovation, and learning
+                resources
+              </p>
+            </div>
+
+            {/* Stat 3 */}
+            <div className="text-center">
+              <p className="text-4xl md:text-5xl lg:text-6xl font-semibold text-accent mb-3 hover:underline hover:underline-offset-4 hover:decoration-6 hover:decoration-accent">
+                156,700+
+              </p>
+              <p className="text-sm md:text-lg uppercase tracking-wide font-mono">
+                Adults empowered through skills development and community
+                programs
+              </p>
+            </div>
+          </div>
         </div>
-        {/* Stats Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-8">
-          {/* Stat 1 */}
-          <div className="text-center">
-            <p className="text-4xl md:text-5xl lg:text-6xl font-semibold text-accent mb-3 hover:underline hover:underline-offset-4 hover:decoration-6 hover:decoration-accent">
-              875,000+
-            </p>
-            <p className="text-sm md:text-lg uppercase tracking-wide font-mono">
-              Children reached through our educational programs and healthcare
-              initiatives
-            </p>
+      </div>
+
+      {/* Our Works Section (in cards) */}
+      <div className="p-1 mt-16">
+        <h2 className="text-3xl font-bold mb-6 text-center uppercase tracking-wide">
+          A Glance at Our Work
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 container">
+          {/* Card 1 */}
+          <div className="overflow-hidden">
+            <Image
+              src="/images/photoshoot/boy-filled-with-joy.jpg"
+              alt="Card Image"
+              width={500}
+              height={300}
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+            <div className="pt-4">
+              <h3 className="text-xl capitalize font-semibold mb-1">
+                Child Education
+              </h3>
+              <p className="text-gray-700 mb-2">
+                Last year, your support helped over 3,000 children in rural
+                Ghana access quality education, learning materials, and
+                mentorship.
+              </p>
+              <Link
+                href="/sponsor-a-child"
+                className="text-accent flex items-center gap-1 hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-accent font-semibold"
+              >
+                Sponsor a child today <MoveRight size={16} />
+              </Link>
+            </div>
           </div>
 
-          {/* Stat 2 */}
-          <div className="text-center">
-            <p className="text-4xl md:text-5xl lg:text-6xl font-semibold text-accent mb-3 hover:underline hover:underline-offset-4 hover:decoration-6 hover:decoration-accent">
-              600+
-            </p>
-            <p className="text-sm md:text-lg uppercase tracking-wide font-mono">
-              Schools supported through construction, renovation, and learning
-              resources
-            </p>
+          {/* Card 2 */}
+          <div className="overflow-hidden">
+            <Image
+              src="/images/photoshoot/computer-lab.JPG"
+              alt="Card Image"
+              width={500}
+              height={300}
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+            <div className="pt-4">
+              <h3 className="text-xl capitalize font-semibold mb-1">
+                Digital Learning Access
+              </h3>
+              <p className="text-gray-700 mb-2">
+                In 2024, we equipped 5 rural schools with computer labs, opening
+                the door to digital education for 1,200 students.
+              </p>
+              <Link
+                href="/healthcare"
+                className="text-accent flex items-center gap-1 hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-accent font-semibold"
+              >
+                Learn more <MoveRight size={16} />
+              </Link>
+            </div>
           </div>
 
-          {/* Stat 3 */}
-          <div className="text-center">
-            <p className="text-4xl md:text-5xl lg:text-6xl font-semibold text-accent mb-3 hover:underline hover:underline-offset-4 hover:decoration-6 hover:decoration-accent">
-              156,700+
+          {/* Card 3 */}
+          <div className="overflow-hidden">
+            <Image
+              src="/images/photoshoot/teachers-teaching-in-class.jpg"
+              alt="Card Image"
+              width={500}
+              height={300}
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+            <div className="pt-4">
+              <h3 className="text-xl capitalize font-semibold mb-1">
+                Empowering Educators
+              </h3>
+              <p className="text-gray-700 mb-2">
+                We trained 150 rural teachers in modern, inclusive teaching
+                practices, impacting thousands of young learners.
+              </p>
+              <Link
+                href="/healthcare"
+                className="text-accent flex items-center gap-1 hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-accent font-semibold"
+              >
+                Learn more <MoveRight size={16} />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <span className="leading-relaxed text-lg mt-4 text-center flex align-middle justify-center">
+          <Link
+            href={"/about"}
+            className="text-accent hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-accent text-center flex gap-1 items-center font-semibold"
+          >
+            Explore more about our work <MoveRight size={16} />
+          </Link>
+        </span>
+      </div>
+
+      {/* devider */}
+      <div className="flex justify-center items-center mt-16">
+        <div className="w-full h-[2px] bg-gray-300" />
+      </div>
+
+      {/* Featured Partner */}
+      <div className="flex flex-col mt-16 text-gray-800 w-full">
+        <div className="text-center leading-snug">
+          <h3 className="text-3xl font-bold mb-2 uppercase tracking-wide">
+            Featured Partner
+          </h3>
+          <p className="text-md text-gray-700 tracking-wide">
+            Together, we&apos;ve helped build brighter futures for children
+            across Ghana
+          </p>
+        </div>
+        {/* Partner Logo */}
+        <div className="flex flex-col sm:flex-row mt-2 items-start container">
+          <Image
+            src="/images/partners/Gov_IntDev_Std_Colour.jpg"
+            alt="Partner Logo"
+            width={300}
+            height={200}
+            className="mt-4"
+          />
+          {/* Partner Description */}
+          <div className="text-left text-gray-700 pt-12 w-ful">
+            <h3 className="text-xl text-accent font-bold mb-2 uppercase tracking-wide">
+              Government of Ireland
+            </h3>
+            <p className="text-md text-gray-700 tracking-wide">
+              We are proud to partner with the Government of Ireland, whose
+              support has been instrumental in our mission to provide quality
+              education and healthcare to children in Ghana. Their commitment to
+              global development aligns with our vision of a world where every
+              child has the opportunity to grow, learn, and prosper. Through
+              this invaluable collaboration, we are implementing innovative
+              projects that enhance local educational systems, fortify community
+              healthcare, and create sustainable economic opportunities. By
+              working together, we are not only bridging the opportunity gap but
+              also empowering communities to build a resilient and promising
+              future for the generations to come.
             </p>
-            <p className="text-sm md:text-lg uppercase tracking-wide font-mono">
-              Adults empowered through skills development and community programs
-            </p>
+            <Link
+              href="/partners"
+              className="text-accent flex items-center gap-1 hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-accent font-semibold mt-4"
+            >
+              Explore our partners <MoveRight size={16} />
+            </Link>
           </div>
         </div>
       </div>
